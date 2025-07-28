b5adeb7d9e2f9ef81e72a150241ad6c645cc55b3
ibnusulthan <ibnusulthanbagaskara@gmail.com>
2025-07-28 14:44:26 +0700

    add exercise git 3 & 4

---
+++ exerciseGit3.ts
@@ -0,0 +1,3 @@
+console.log('File git 3 nih');
+console.log('This is exerciseGit3.ts');
+console.log('Learning Git with TypeScript!');
---
+++ exerciseGit4.ts
@@ -0,0 +1,2 @@
+console.log('ini file exerciseGit4.ts');
+console.log('This file is part of the Day3 exercises.');
